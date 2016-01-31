ec776b8a37e1692bca227bebea044dd43c61fee9
nouchka <github@katagena.com>
2016-01-31 15:12:05 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# docker-cloudflare-dyndns
+
+GIST https://gist.github.com/benkulbertis/fff10759c2391b6618dd
+
+Using same docker env from https://github.com/msh100/Docker-Images/tree/master/cloudflare-dyndns
+
+Use with docker compose
+
---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! openssl sha1 /cloudflare-update-record.sh| grep $CLOUDFLARE_FILE_SHA >> /dev/null 2>&1; then
+	echo "wrong SHA"
+	exit 1
+fi
+
+ZONE=${CF_HOST#*.}
+
+sed -i \
+    -e "s/auth_email=.*/auth_email=\"$CF_EMAIL\"/g" \
+    -e "s/auth_key=.*/auth_key=\"$CF_API\"/g" \
+    -e "s/zone_name=.*/zone_name=\"$ZONE\"/g" \
+    -e "s/record_name=.*/record_name=\"$CF_HOST\"/g" \
+    /cloudflare-update-record.sh
+
+/cloudflare-update-record.sh
