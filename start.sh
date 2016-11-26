412c60e9616b13e7dc01a7107a6f8fed106c6308
nouchka <github@katagena.com>
2016-11-26 18:33:12 +0100

    CI

--- start.sh
+++ start.sh
@@ -2,6 +2,7 @@
 
 if ! openssl sha1 /cloudflare-update-record.sh| grep $CLOUDFLARE_FILE_SHA >> /dev/null 2>&1; then
 	echo "wrong SHA"
+	openssl sha1 /cloudflare-update-record.sh
 	exit 1
 fi
 
@@ -12,11 +13,12 @@ if [ ! "$CF_API" ]; then
 	exit 0
 fi
 
-sed -i \
-    -e "s/auth_email=.*/auth_email=\"$CF_EMAIL\"/g" \
-    -e "s/auth_key=.*/auth_key=\"$CF_API\"/g" \
-    -e "s/zone_name=.*/zone_name=\"$ZONE\"/g" \
-    -e "s/record_name=.*/record_name=\"$CF_HOST\"/g" \
-    /cloudflare-update-record.sh
-
-/cloudflare-update-record.sh
+if [ ! "$TESTING" ]; then
+	sed -i \
+		-e "s/auth_email=.*/auth_email=\"$CF_EMAIL\"/g" \
+		-e "s/auth_key=.*/auth_key=\"$CF_API\"/g" \
+		-e "s/zone_name=.*/zone_name=\"$ZONE\"/g" \
+		-e "s/record_name=.*/record_name=\"$CF_HOST\"/g" \
+		/cloudflare-update-record.sh
+	/cloudflare-update-record.sh
+fi
