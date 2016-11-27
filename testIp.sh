ef20c6a357b508b3d1825c19cf79eceadf22bb2e
nouchka <github@katagena.com>
2016-11-27 18:24:24 +0100

    Test ip (assuming that travis IP change between builds ???)

---
+++ testIp.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ip=$(curl -s http://ipv4.icanhazip.com)
+cl_ip=$(nslookup testing.musaeum.info| awk '/^Address: / { print $2 ; exit }')
+
+if [ $ip != $cl_ip ]; then
+	echo "IP don't match. CL IP: "$cl_ip" CURRENT_IP: "$ip" "
+	exit 1
+fi
+
+echo "IP good ("$cl_ip")"
