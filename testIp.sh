b77420a38ffb8ad1ea60fe7e129e2943cd09cf86
nouchka <github@katagena.com>
2016-11-27 18:28:11 +0100

    DNS time to update

--- testIp.sh
+++ testIp.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+##Let the time to DNS to propagate
+sleep 50
+
 ip=$(curl -s http://ipv4.icanhazip.com)
 cl_ip=$(nslookup testing.musaeum.info| awk '/^Address: / { print $2 ; exit }')
 
