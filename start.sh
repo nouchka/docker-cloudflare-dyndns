d3c2718011fb00e20d5fbc7eca7cc1484267e420
nouchka <github@katagena.com>
2016-11-27 17:53:26 +0100

    Testing with true domain

--- start.sh
+++ start.sh
@@ -7,12 +7,10 @@ if [ ! "$CF_API" ]; then
 	exit 0
 fi
 
-if [ ! "$TESTING" ]; then
-	sed -i \
-		-e "s/auth_email=.*/auth_email=\"$CF_EMAIL\"/g" \
-		-e "s/auth_key=.*/auth_key=\"$CF_API\"/g" \
-		-e "s/zone_name=.*/zone_name=\"$ZONE\"/g" \
-		-e "s/record_name=.*/record_name=\"$CF_HOST\"/g" \
-		/cloudflare-update-record.sh
+sed -i \
+	-e "s/auth_email=.*/auth_email=\"$CF_EMAIL\"/g" \
+	-e "s/auth_key=.*/auth_key=\"$CF_API\"/g" \
+	-e "s/zone_name=.*/zone_name=\"$ZONE\"/g" \
+	-e "s/record_name=.*/record_name=\"$CF_HOST\"/g" \
 	/cloudflare-update-record.sh
-fi
+/cloudflare-update-record.sh
