c307719c747ad2dd3095645d71ee5162f5587f42
Kyle Lucy <kmlucy@gmail.com>
2017-01-12 22:54:49 -0500

    use https to find IP address

--- start.sh
+++ start.sh
@@ -12,5 +12,6 @@ sed -i \
 	-e "s/auth_key=.*/auth_key=\"$CF_API\"/g" \
 	-e "s/zone_name=.*/zone_name=\"$ZONE\"/g" \
 	-e "s/record_name=.*/record_name=\"$CF_HOST\"/g" \
+	-e "s=http://ipv4.icanhazip.com=https://ipv4.icanhazip.com=g" \
 	/cloudflare-update-record.sh
 /cloudflare-update-record.sh
